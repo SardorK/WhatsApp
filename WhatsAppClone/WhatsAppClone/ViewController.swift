3a0c4851ddd4c1e3daac4ad42353809081c8dbf8
Karshiev Sardor <91384924+SardorK@users.noreply.github.com>
2023-02-07 19:13:22 +0500

    WhatsApp File Cretae

---
+++ WhatsAppClone/WhatsAppClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsAppClone
+//
+//  Created by User 2 on 06/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
